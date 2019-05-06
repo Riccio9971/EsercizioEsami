4ee171a79664fdb12b90d8740e40e771106f1fb1
Antonio Riccio <riccio19971@gmail.com>
2019-05-06 11:34:54 +0200

    Prima commit

---
+++ src/Esame.java
@@ -0,0 +1,7 @@
+
+public class Esame 
+{
+			Studente studente;
+			String materia;
+			int voto;
+}
---
+++ src/Persona.java
@@ -0,0 +1,9 @@
+
+public class Persona 
+{
+		
+		String nome;
+		String cognome;
+		int eta;
+
+}
